cacb0571b09c9ebbf4d7e7f8d3d4de4166227b35
FoxMindTech <foxmindtechteam@gmail.com>
2025-08-03 20:02:55 +0500

    favicon and env set

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
           href="https://fonts.gstatic.com"
           crossOrigin=""
         />
+        <link rel="icon" href="/br.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Space+Grotesk:wght@300..700&display=swap"
           rel="stylesheet"
